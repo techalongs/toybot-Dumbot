8665b7d0caa4b99f74520b3148190107074a58ff
codagirlaz <eleanor_lam@outlook.com>
2025-11-07 18:29:59 -0700

    Slowed bot for outreach; added separate drive practice opMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveDumbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveDumbot.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Walking Dumbot - Outreach")
-public class WalkingDumbot extends LinearOpMode {
+@TeleOp(name = "Walking Dumbot - Drive Practice")
+public class DriveDumbot extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(hardwareMap);
@@ -15,7 +15,9 @@ public class WalkingDumbot extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            robot.drive(gamepad1, 0.5);
+            if (gamepad1.right_bumper) robot.drive(gamepad1, 1);
+            else if (gamepad1.left_bumper) robot.drive(gamepad1, 0.5);
+            else robot.drive(gamepad1, 0.8);
             robot.controlArm(gamepad1, 1);
             robot.displayData(telemetry);
         }
